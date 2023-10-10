0a1d68520e0bed95309790d437f1aae0afbed002
Cruz06 <olarut72@gmail.com>
2023-10-10 08:52:32 +0300

    add test logout

---
+++ lesson1/test_logout.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+
+def test_logout():
+    driver.get("https://www.saucedemo.com/")
+    url_before = driver.current_url
+    #print(url_before)
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("standard_user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("secret_sauce")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+    # в левой части есть меню магазина, там команда logout
+    burger_menu = driver.find_element(By.ID, "react-burger-menu-btn")
+    burger_menu.click()
+    time.sleep(3)
+
+    logout = driver.find_element(By.CSS_SELECTOR, "#logout_sidebar_link")
+    logout.click()
+    time.sleep(1)
+
+    # если вышел , то возврат на начальную страницу, можно проверить через УРЛ
+    url_after = driver.current_url
+    #print(url_after)
+
+    assert url_after == url_before
+    driver.quit()       #когда будет закрывающая фикстура, то удалить

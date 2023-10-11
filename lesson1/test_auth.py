9b29712032e21239a5fe11819b25925a7e3284eb
Olga Gerber <gerberolya@gmail.com>
2023-10-11 10:49:33 +0200

    add test with uncorrect login and pass

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 
 
 
-def test_login_form():
+def test_login_form_correct():
     driver.get("https://www.saucedemo.com/")
 
     username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
@@ -18,8 +18,27 @@ def test_login_form():
     login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
     login_button.click()
 
-    time.sleep(5)
+    time.sleep(3)
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+
+    driver.quit()
+
+def test_login_form_uncorrect():
+    driver.get("https://www.saucedemo.com/")
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("user")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+
+    time.sleep(3)
+    error_button_text = driver.find_element(By.CSS_SELECTOR, 'h3[data-test="error"]').text
+
+    assert error_button_text == 'Epic sadface: Username and password do not match any user in this service'
 
     driver.quit()
 

760693f40c067121e0a38ab5b4e98d1d1f8a71f1
Nariman Mirzakhanov <mnariman05@gmail.com>
2023-10-11 01:01:04 -0700

    add test_add_to_cart.py

---
+++ lesson1/test_add_to_cart.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+
+def test_add_item_in_the_cart():
+    driver.get("https://www.saucedemo.com/")
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("standard_user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("secret_sauce")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+
+    item = driver.find_element(By.XPATH, '//*[@id="item_1_title_link"]/div[@class="inventory_item_name"]').text
+
+    add_to_cart_button = driver.find_element(By.XPATH, '//*[@data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
+    add_to_cart_button.click()
+
+    shopping_cart = driver.find_element(By.XPATH, '//*[@class="shopping_cart_link"]')
+    shopping_cart.click()
+
+    item_in_the_cart = driver.find_element(By.XPATH, '//*[@id="item_1_title_link"]/div[@class="inventory_item_name"]').text
+    assert item == item_in_the_cart

6da6022e421277bf6edadbea676877af48804c07
Nariman Mirzakhanov <mnariman05@gmail.com>
2023-10-11 23:29:36 -0700

    modify test_add_to_cart

--- lesson1/test_add_to_cart.py
+++ lesson1/test_add_to_cart.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -17,13 +19,13 @@ def test_add_item_in_the_cart():
     login_button.click()
 
     item = driver.find_element(By.XPATH, '//*[@id="item_1_title_link"]/div[@class="inventory_item_name"]').text
-
+    time.sleep(2)
     add_to_cart_button = driver.find_element(By.XPATH, '//*[@data-test="add-to-cart-sauce-labs-bolt-t-shirt"]')
     add_to_cart_button.click()
-
+    time.sleep(2)
     shopping_cart = driver.find_element(By.XPATH, '//*[@class="shopping_cart_link"]')
     shopping_cart.click()
-
+    time.sleep(2)
     item_in_the_cart = driver.find_element(By.XPATH, '//*[@id="item_1_title_link"]/div[@class="inventory_item_name"]').text
     assert item == item_in_the_cart
 

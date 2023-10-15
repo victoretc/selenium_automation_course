acc07589b6e6534229aa66267e995fb07b864a80
Olga Gerber <gerberolya@gmail.com>
2023-10-15 12:58:33 +0200

    add test_remove_item_from_the_cart

--- lesson1/test_cart.py
+++ lesson1/test_cart.py
@@ -18,7 +18,35 @@ def test_add_item_from_catalog():
 
     first_item_add_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')
     first_item_add_button.click()
+    time.sleep(2)
 
     item_in_the_cart = driver.find_element(By.XPATH, "//span[@class='shopping_cart_badge']")
+    time.sleep(2)
     #assert item_in_the_cart.is_displayed()
     assert item_in_the_cart.text == '1'
+
+def test_remove_item_from_the_cart():
+    driver.get("https://www.saucedemo.com/")
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("standard_user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("secret_sauce")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+
+    first_item_add_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')
+    first_item_add_button.click()
+
+    cart_button = driver.find_element(By.XPATH, "//span[@class='shopping_cart_badge']")
+    cart_button.click()
+    time.sleep(2)
+
+    remove_button = driver.find_element(By.XPATH,'//button[@data-test="remove-sauce-labs-backpack"]')
+    remove_button.click()
+    time.sleep(2)
+
+    assert driver.find_elements(By.XPATH, '//div[@class="removed_cart_item"]')
+

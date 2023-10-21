f892c089e311cd6c056c5084301c5dff3f3c4f57
Evgenii_K <117931868+evg-QA@users.noreply.github.com>
2023-10-21 23:33:40 +0300

    add conftest file (#4)

---
+++ lesson2/hw_2/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    print('\nquit browser...')
+    driver.quit()

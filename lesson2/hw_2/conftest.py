9c149049454b3d7de746a4e233995cdd4972d58e
Evgenii_K <117931868+evg-QA@users.noreply.github.com>
2023-10-23 23:38:15 +0300

    Delete lesson2/hw_2/conftest.py

--- lesson2/hw_2/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture()
-def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    print('\nquit browser...')
-    driver.quit()

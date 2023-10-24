63230b75b151d5e7d536139f846e028653e52c10
evg-QA <freecadelka@yahoo.com>
2023-10-24 20:52:14 +0300

    add locators file

---
+++ lesson3/hw_3/locators.py
@@ -0,0 +1,13 @@
+class AuthLocators:
+    AUTH_H1_TEXT = "//h1[text()='Практика с ожиданиями в Selenium']"
+    START_TEST_BUTTON = "//button[@id='startTest']"
+    REGISTRATION_FORM = "//div[@id='registrationForm']"
+    LOGIN_FORM_TEXT = "//label[@for='login']"
+    PASSWORD_FORM_TEXT = "//label[@for='password']"
+    INPUT_LOGIN = "//input[@id='login']"
+    INPUT_PASSWORD = "//input[@id='password']"
+    AGREE_CHECKBOX = "//input[@id='agree']"
+    REGISTRATION_BUTTON = "//button[@id='register']"
+    LOADER = "//div[@id='loader']"
+    SUCCESS_MESSAGE = "//p[@id='successMessage']"
+

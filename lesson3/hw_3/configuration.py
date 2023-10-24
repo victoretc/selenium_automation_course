520a963a69bb8d22f123778ec37ec308926fb1a1
evg-QA <freecadelka@yahoo.com>
2023-10-24 20:51:03 +0300

    add configuration file

---
+++ lesson3/hw_3/configuration.py
@@ -0,0 +1,26 @@
+"""All constants that relate to tests"""
+
+class BaseUrls:
+    """----------urls----------"""
+
+    """auth page"""
+    AUTH_URL = 'https://victoretc.github.io/selenium_waits/'
+
+
+class LocatorsText:
+    """----------text----------"""
+
+    """auth page"""
+    H1_HEADER_TEXT = 'Практика с ожиданиями в Selenium'
+    LOGIN_TEXT = 'Логин:'
+    PASSWORD_TEXT = 'Пароль:'
+    SUCCESS_MESSAGE_TEXT = 'Вы успешно зарегистрированы!'
+
+
+class TestData:
+    """----------valid test data----------"""
+
+    """auth page"""
+    LOGIN = 'логин'
+    PASSWORD = 'пароль'
+

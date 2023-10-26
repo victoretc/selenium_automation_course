aa2a056025de28550a10b46ff3e854454bdcf7ee
Denis Stepulenok <super.denis@gmail.com>
2023-10-26 16:48:15 +0300

    options = util.chrome_options()

--- lesson3/test_options.py
+++ lesson3/test_options.py
@@ -1,10 +1,12 @@
 import pytest
 from selenium import webdriver
 
+import util
+
 
 @pytest.fixture
 def chrome_options():
-    options = chrome_options()
+    options = util.chrome_options()
     # options.add_argument('--window-size=100,100')
     options.add_argument('--incognito')
     return options

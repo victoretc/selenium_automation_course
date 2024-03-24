b5762dda922c9da4120d436825c3d59d362ccc35
Витя Ярошук <vitaarosuk@MacBook-Air-Vita.local>
2024-03-24 03:03:54 +0300

    add lesson 1

---
+++ lesson1/test-case.md
@@ -0,0 +1,7 @@
+# Авторизация
+
+1. Открыть url: <https://www.saucedemo.com/v1/>
+2. Ввести "standard_user" в "Username"
+3. Ввести "secret_sauce" в "Password"
+4. Кликнуть "Login"
+5. Проверить, что мы перешли на новый url: <https://www.saucedemo.com/v1/inventory.html>
--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -1,33 +1,18 @@
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.by import By
-import time 
 
-driver = webdriver.Chrome()
+browser = webdriver.Chrome()
 
+def test_auth_positive():
+    browser.get('https://www.saucedemo.com/v1/')
 
-
-def test_login_form():
-    driver.get("https://www.saucedemo.com/")
-
-    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    username_field.send_keys("standard_user")
-
-    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    password_field.send_keys("secret_sauce")
-
-    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    login_button.click()
-
-    time.sleep(5)
-    assert driver.current_url == "https://www.saucedemo.com/inventory.html"
-
-    driver.quit()
+    browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
+    browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
+    browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    assert browser.current_url == 'https://www.saucedemo.com/v1/inventory.html', 'url не соответствует ожидаемому'
 
 
-
-
-
-
+    browser.quit()
 
 
 

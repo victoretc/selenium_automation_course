0249e3b70061a6ad5ca0f3e8c3936adcd984c59b
Витя Ярошук <vitaarosuk@MacBook-Air-Vita.local>
2024-04-09 00:49:24 +0300

    add lesson3

---
+++ lesson3/test_options.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+@pytest.fixture
+def chrome_options():
+    options = Options()
+    options.add_argument('--window-size=100,100')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    return options
+
+@pytest.fixture
+def driver(chrome_options):
+    driver = webdriver.Chrome(options=chrome_options)
+    yield driver
+    driver.quit()
+
+def test_example(driver):
+    driver.get('https://www.saucedemo.com/')
+    assert driver.current_url == 'https://www.saucedemo.com/'
+ 
